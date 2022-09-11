5abb87d7cce44dc710a35970590bc9422aa97fe8
Judea Dumont <dumontjudea2@gmail.com>
2022-09-11 06:09:14 -0600

    - Refactored Generic Hibernate Driver to run a lambda in-between opening and closing a session  - its insane how convenient functional programming is in Java, trying to do this in plain javascript breaks encapsulation rules or something like that  - I'm so not convinced its this easy to work out that I'm going to redo it in a brand-new checkout and write a readme

--- webapi/src/main/java/com/webapi/webapi/databasedrivers/hibernateinmemory/HibernateConnectionGenericMethods.java
+++ webapi/src/main/java/com/webapi/webapi/databasedrivers/hibernateinmemory/HibernateConnectionGenericMethods.java
@@ -8,46 +8,11 @@ import java.util.List;
 
 //todo: refactor methods to use a lambda that opens and closes a session before and after it
 public class HibernateConnectionGenericMethods {
+
+    private static Session session = null;
+
     private HibernateConnectionGenericMethods() {
-    }
 
-    public static <T> void genericSave(T obj) {
-        Session session = HibernateConnection.getSession();
-        session.beginTransaction();
-        session.persist(obj);
-        session.getTransaction().commit();
-        session.close();
-    }
-
-    public static <T extends ID> Long genericSaveReturnID(T obj) {
-        Session session = HibernateConnection.getSession();
-        session.beginTransaction();
-        session.persist(obj);
-        session.getTransaction().commit();
-        session.close();
-        return obj.getId();
-    }
-
-    public static <T> T genericGet(Class<T> classOfObj, Long id) throws ClassNotFoundException {
-
-        Session session = HibernateConnection.getSession();
-        session.beginTransaction();
-        T obj = session.get(classOfObj, id);
-        session.getTransaction().commit();
-        session.close();
-        return obj;
-    }
-
-    public static <T extends ID> List<T> genericGetByName(Class<T> classOfObject, String name) {
-        Session session = HibernateConnection.getSession();
-        session.beginTransaction();
-
-        Query<T> tQuery = session.createQuery(
-                "from " + genericGetTableNameFromClass(classOfObject) + " c where c.name = '" + name + "'", classOfObject);
-
-        List<T> resultList = tQuery.getResultList();
-        session.close();
-        return resultList;
     }
 
     private static <T extends ID> String genericGetTableNameFromClass(Class<T> classOfObject) {
@@ -55,33 +20,68 @@ public class HibernateConnectionGenericMethods {
         return packageLocation[packageLocation.length - 1];
     }
 
-    public static <T extends ID> void genericUpdate(T object) {
-
-        Session session = HibernateConnection.getSession();
-        session.beginTransaction();
-        session.merge(object);
-        session.getTransaction().commit();
-        session.close();
+    private interface RunIt<R> {
+        R op();
     }
 
-    public static <T extends ID> void genericDelete(T object) {
-
-        Session session = HibernateConnection.getSession();
+    public static <R> R runInSession(RunIt<R> runIt) {
+        session = HibernateConnection.getSession();
         session.beginTransaction();
-        session.remove(object);
+        R result = runIt.op();
         session.getTransaction().commit();
         session.close();
+        return result;
+    }
+
+    public static <T> void genericSave(T obj) {
+        runInSession(() -> {
+            session.persist(obj);
+            return null;
+        });
+    }
+
+    public static <T extends ID> Long genericSaveReturnID(T obj) {
+        runInSession(() -> {
+            session.persist(obj);
+            return null;
+        });
+        //todo: hibernate knows to cram the object with the ID that gets generated, we have to tell the sql result from postgres to do this
+        return obj.getId();
+    }
+
+    public static <T> T genericGet(Class<T> classOfObj, Long id) {
+        return runInSession(() -> session.get(classOfObj, id));
+    }
+
+    public static <T extends ID> List<T> genericGetByName(Class<T> classOfObject, String name) {
+        return runInSession(() -> {
+            Query<T> tQuery = session.createQuery(
+                    "from " + genericGetTableNameFromClass(classOfObject) + " c where c.name = '" + name + "'", classOfObject);
+
+            return tQuery.getResultList();
+        });
+    }
+
+    public static <T extends ID> void genericUpdate(T obj) {
+        runInSession(() -> {
+            session.merge(obj);
+            return null;
+        });
+    }
+
+    public static <T extends ID> void genericDelete(T obj) {
+        runInSession(() -> {
+            session.remove(obj);
+            return null;
+        });
     }
 
     public static <T extends ID> List<T> genericGetAll(Class<T> classOfObject) {
-        Session session = HibernateConnection.getSession();
-        session.beginTransaction();
+        return runInSession(() -> {
+            Query<T> tQuery = session.createQuery(
+                    "from " + genericGetTableNameFromClass(classOfObject), classOfObject);
 
-        Query<T> tQuery = session.createQuery(
-                "from " + genericGetTableNameFromClass(classOfObject), classOfObject);
-
-        List<T> resultList = tQuery.getResultList();
-        session.close();
-        return resultList;
+            return tQuery.getResultList();
+        });
     }
 }
