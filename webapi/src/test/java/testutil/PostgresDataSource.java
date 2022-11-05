6537e68c0fe4708fa92c0d8fcd8c8ce910087789
Judea Dumont <dumontjudea2@gmail.com>
2022-11-04 23:11:25 -0600

    Integrating flyway into project - psql does not use capital letters for creating DB (that's hella stupid), project now conforms - github action steps are named better - you have to make the initial SQL V1.1 because baseline takes V1 -

--- webapi/src/test/java/testutil/PostgresDataSource.java
+++ webapi/src/test/java/testutil/PostgresDataSource.java
@@ -7,7 +7,7 @@ import java.util.Properties;
 
 public class PostgresDataSource {
     private static final String DRIVER = "org.postgresql.Driver";
-    private static final String JDBC_URL = "jdbc:postgresql://postgres:5432/sampleDB";
+    private static final String JDBC_URL = "jdbc:postgresql://postgres:5432/sampledb";
     public static final String USERNAME = "postgres";
     private static final String PASSWORD = "root";
 
