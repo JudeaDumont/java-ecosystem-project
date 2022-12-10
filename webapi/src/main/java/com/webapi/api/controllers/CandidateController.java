14415939953d0d4b0451340084e985b16aba8d50
JudeaDumont <dumontjudea2@gmail.com>
2022-12-10 15:58:29 -0700

    need info

--- webapi/src/main/java/com/webapi/api/controllers/CandidateController.java
+++ webapi/src/main/java/com/webapi/api/controllers/CandidateController.java
@@ -39,6 +39,7 @@ public class CandidateController {
     @GetMapping
     public ResponseEntity<Response>
     getAll() {
+        System.out.println(candidateService.getAll().toString());
         return ResponseEntity.ok(
                 Response.builder()
                         .timeStamp(now())
